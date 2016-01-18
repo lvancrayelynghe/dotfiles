d41c289eaf9755492b618fea530415de37f36b38
Benoth <benoth83@gmail.com>
2016-01-18 13:46:55 +0100

    Added install script and instructions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dotfiles
+# Dotfiles
 Personnal dotfiles
+
+## Installation
+```
+curl --silent https://raw.githubusercontent.com/Benoth/dotfiles/master/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Clone & config repository
+cd
+git clone --recursive git://github.com/Benoth/dotfiles.git
+cd dotfiles
+git config --local user.email "benoth83@gmail.com"
+git config --local user.name "Benoth"
+
+# Create the symlinks
+symlink-dotfiles.sh
