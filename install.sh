3fe66553bb2b4033229fe47f9b320414c8c3e7b3
Benoth <benoth83@gmail.com>
2016-01-18 13:50:37 +0100

    Debug script execution

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@
 cd
 git clone --recursive git://github.com/Benoth/dotfiles.git
 cd dotfiles
+chmod u+x *.sh
 git config --local user.email "benoth83@gmail.com"
 git config --local user.name "Benoth"
 
 # Create the symlinks
-symlink-dotfiles.sh
+./symlink-dotfiles.sh
