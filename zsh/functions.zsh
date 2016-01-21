9ff2a1fe469da2b8841ad7c4f7a2b8faff5c9a0a
Benoth <benoth83@gmail.com>
2016-01-21 23:10:10 +0100

    Add highlight function and aliases

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -88,6 +88,22 @@ function grep-passthru {
     fi
 }
 
+# Highlight a match in given color
+function highlight() {
+    declare -A fg_color_map
+    fg_color_map[black]=30
+    fg_color_map[red]=31
+    fg_color_map[green]=32
+    fg_color_map[yellow]=33
+    fg_color_map[blue]=34
+    fg_color_map[magenta]=35
+    fg_color_map[cyan]=36
+
+    fg_c=$(echo -e "\e[1;${fg_color_map[$1]}m")
+    c_rs=$'\e[0m'
+    sed -u s"/$2/$fg_c\0$c_rs/g"
+}
+
 # Commands usage statistics
 function history-stats() {
 	fc -l 1 | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n25
