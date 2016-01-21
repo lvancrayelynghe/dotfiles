ce7bbc2a359a82694aa03ed3e6006cc1cb003139
Benoth <benoth83@gmail.com>
2016-01-21 14:20:07 +0100

    Add missing NPM config

---
+++ zsh/npm.zsh
@@ -0,0 +1,10 @@
+NPM_PACKAGES="${HOME}/.npm-packages"
+
+PATH="$NPM_PACKAGES/bin:$PATH"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
+npm config set prefix $NPM_PACKAGES
+
