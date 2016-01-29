010100e2296e562e49e47aa02f1a5184bbd30334
Benoth <benoth83@gmail.com>
2016-01-29 14:22:16 +0100

    Small git tuning

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -158,7 +158,7 @@ alias gss='git stash save'
 alias gsa='git stash apply'
 alias gsp='git stash pop'
 alias gsl='git stash list'
-alias cdiff='cdiff -s -w 0'
+alias cgd='cdiff -s -w 0' ## columned & colored git diff
 
 # Local rsync
 alias rsync-copy="rsync -av --progress -h"
