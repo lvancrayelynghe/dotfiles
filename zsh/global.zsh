b6f19754da186090918c757891a9c2d5b6dd192f
Benoth <benoth83@gmail.com>
2016-04-12 12:56:01 +0200

    Remove ZSH useless configs

--- zsh/global.zsh
+++ zsh/global.zsh
@@ -6,9 +6,7 @@ setopt auto_resume          # Attempt to resume existing job before creating a n
 setopt notify               # Report status of background jobs immediately.
 unsetopt bg_nice            # Don't run all background jobs at a lower priority.
 unsetopt hup                # Don't kill jobs on shell exit.
-unsetopt check_jobs         # Don't report on jobs when shell exit.
 setopt nolistbeep           # don't beep on ambiguous completion \o/
-setopt no_beep              # don't beep on error
 setopt no_beep              # don't beep on error
 setopt interactive_comments # Allow comments even in interactive shells
 setopt multios              # Perform implicit tees or cats when multiple redirections are attempted, see http://zsh.sourceforge.net/Doc/Release/Options.html#index-MULTIOS
