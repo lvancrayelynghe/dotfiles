6ddb056fbe2f3fb1fa181c91ca43d7d6b000b759
Benoth <benoth83@gmail.com>
2016-05-02 22:01:35 +0200

    Fix theme colors

--- scripts/gnome-terminal-tomorrow-night.sh
+++ scripts/gnome-terminal-tomorrow-night.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+#
+# See https://terminal.sexy/#HR8hxcjGHR8hsDo-d4kAtYwpKjXSfUCwPIpukpWTlpiWzGZmtb1o8MZ0gaK-spS7ir637Ovs
+# Source https://github.com/chriskempson/tomorrow-theme/blob/master/Gnome-Terminal/setup-theme.sh
+#
 
-[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME=Tomorrow
-[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG=Tomorrow
+[[ -z "$PROFILE_NAME" ]] && PROFILE_NAME=TomorrowNight
+[[ -z "$PROFILE_SLUG" ]] && PROFILE_SLUG=TomorrowNight
 [[ -z "$DCONF" ]] && DCONF=dconf
 [[ -z "$UUIDGEN" ]] && UUIDGEN=uuidgen
 
@@ -57,10 +61,10 @@ if which "$DCONF" > /dev/null 2>&1; then
 
         # update profile values with theme options
         dset visible-name "'$PROFILE_NAME'"
-        dset palette "['rgb(0,0,0)','rgb(145,34,38)','rgb(119,137,0)','rgb(174,123,0)','rgb(29,37,148)','rgb(104,42,155)','rgb(43,102,81)','rgb(146,149,147)','rgb(102,102,102)','rgb(204,102,102)','rgb(181,189,104)','rgb(240,198,116)','rgb(129,182,190)','rgb(178,148,187)','rgb(138,190,183)','rgb(236,235,236)']"
-        dset background-color "'rgb(29,31,33)'"
-        dset foreground-color "'rgb(197,200,198)'"
-        dset bold-color "'rgb(138,186,183)'"
+        dset palette "['#1d1f21', '#b03a3e', '#778900', '#b58c29', '#2a35d2', '#7d40b0', '#3c8a6e', '#929593', '#969896', '#cc6666', '#b5bd68', '#f0c674', '#81a2be', '#b294bb', '#8abeb7', '#ecebec']"
+        dset background-color "'#1d1f21'"
+        dset foreground-color "'#c5c8c6'"
+        dset bold-color "'#c5c8c6'"
         dset bold-color-same-as-fg "false"
         dset use-theme-colors "false"
 
@@ -110,10 +114,10 @@ glist_append() {
 glist_append string /apps/gnome-terminal/global/profile_list "$PROFILE_SLUG"
 
 gset string visible_name "$PROFILE_NAME"
-gset string palette "#000000000000:#919122222626:#777789890000:#AEAE7B7B0000:#1D1D25259494:#68682a2a9b9b:#2B2B66665151:#929295959393:#666666666666:#CCCC66666666:#B5B5BDBD6868:#F0F0C6C67474:#8181A2A2BEBE:#B2B29494BBBB:#8A8ABEBEB7B7:#ECECEBEBECEC"
-gset string background_color "#1d1d1f1f2121"
-gset string foreground_color "#c5c5c8c8c6c6"
-gset string bold_color "#8A8ABEBEB7B7"
+gset string palette "#1d1f21:#b03a3e:#778900:#b58c29:#2a35d2:#7d40b0:#3c8a6e:#929593:#969896:#cc6666:#b5bd68:#f0c674:#81a2be:#b294bb:#8abeb7:#ecebec"
+gset string background_color "#1d1f21"
+gset string foreground_color "#c5c8c6"
+gset string bold_color "#c5c8c6"
 gset bool   bold_color_same_as_fg "false"
 gset bool   use_theme_colors "false"
 gset bool   use_theme_background "false"
