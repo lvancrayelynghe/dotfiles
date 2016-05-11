667dcfe1180caaae5d2cb30dd51b3ada0df24312
Benoth <benoth83@gmail.com>
2016-05-11 14:30:10 +0200

    Fix zplug handling

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,7 +1,9 @@
+export ZPLUG_HOME=~/.zplug
 if [ ! -f ~/.zplug/zplug ]; then
     echo "$fg_bold[yellow]Installing zplug...$reset_color"
     mkdir -p ~/.zplug
-    wget -q https://git.io/zplug -O ~/.zplug/zplug
+    git clone https://github.com/zplug/zplug $ZPLUG_HOME
+    source $ZPLUG_HOME/init.zsh
     chmod u+x ~/.zplug/zplug
     echo "$fg_bold[yellow]Done ! $reset_color\n"
     source ~/.zplug/zplug
@@ -10,31 +12,36 @@ else
     source ~/.zplug/zplug
 fi
 
-zplug "b4b4r07/zplug", from:github
+ZPLUG_VERSION=`zplug --version`
 
-zplug "rimraf/k", from:github, as:plugin
+if [[ $ZPLUG_VERSION == 2* ]]; then
+    zplug "b4b4r07/zplug", from:github
+    zplug "rimraf/k", from:github, as:plugin
+    zplug "jamesob/desk", from:github, as:command, use:"desk"
+    zplug "raylee/tldr", from:github, as:command, use:"tldr"
+    zplug "junegunn/fzf-bin", as:command, from:gh-r, rename-to:fzf, use:"*linux*amd64*"
+    zplug "Russell91/sshrc", from:github, as:command, use:"sshrc"
+    zplug "ymattw/cdiff", from:github, as:command, use:"cdiff.py", rename-to:"cdiff", at:0.9.8
+    zplug "0xAX/git-cheat", from:github, as:command, use:"git-cheat"
+    zplug "knu/z", from:github, as:plugin, use:z.sh, nice:10
+    zplug "zsh-users/zsh-syntax-highlighting", from:github, nice:10
+else
+    zplug "b4b4r07/zplug", from:github
+    zplug "rimraf/k", from:github, as:plugin
+    zplug "jamesob/desk", from:github, as:command, of:"desk"
+    zplug "raylee/tldr", from:github, as:command, of:"tldr"
+    zplug "junegunn/fzf-bin", as:command, from:gh-r, file:fzf, of:"*linux*amd64*"
+    zplug "Russell91/sshrc", from:github, as:command, of:"sshrc"
+    zplug "ymattw/cdiff", from:github, as:command, of:"cdiff.py", file:"cdiff", at:0.9.8
+    zplug "0xAX/git-cheat", from:github, as:command, of:"git-cheat"
+    zplug "knu/z", from:github, as:plugin, of:z.sh, nice:10
+    zplug "zsh-users/zsh-syntax-highlighting", from:github, nice:10
+fi
 
-zplug "jamesob/desk", from:github, as:command, of:"desk"
+_Z_DATA="$HOME/.cache/z-directories-trackfile" # Source after compinit to enable completion
 
-zplug "raylee/tldr", from:github, as:command, of:"tldr"
-
-zplug "junegunn/fzf-bin", as:command, from:gh-r, file:fzf, of:"*linux*amd64*"
-
-zplug "Russell91/sshrc", from:github, as:command, of:"sshrc"
-
-zplug "ymattw/cdiff", from:github, as:command, of:"cdiff.py", file:"cdiff", at:0.9.8
-
-zplug "0xAX/git-cheat", from:github, as:command, of:"git-cheat"
-
-# Actually doesn't work :( tested : master/1.3/1.3.1
-# zplug "vifon/deer", from:github, at:v1.3.1, of:deer
-
-# Source after compinit to enable completion
-_Z_DATA="$HOME/.cache/z-directories-trackfile"
-zplug "knu/z", from:github, as:plugin, of:z.sh, nice:10
-
-zplug "zsh-users/zsh-syntax-highlighting", from:github, nice:10
-
-zplug install
+if ! zplug check; then
+    zplug install
+fi
 
 zplug load
