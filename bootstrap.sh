b512af0fcde0617d051f337757315546494077e2
Benoth <benoth83@gmail.com>
2016-05-31 14:14:32 +0200

    Add new script to launch or focus the gnome terminal

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,6 +33,7 @@ declare -A SYMLINKS=(
     ['others/remarkable.settings']='.remarkable/remarkable.settings'
     ['scripts/app-starter.py']='bin/app-starter'
     ['scripts/chromium-focus-and-new-tab.sh']='bin/chromium-focus-and-new-tab'
+    ['scripts/terminal-launch-or-focus.sh']='bin/terminal-launch-or-focus'
     ['sublime-text-3/Packages/PHP']='.config/sublime-text-3/Packages/PHP'
     ['sublime-text-3/Packages/User/anmol.tmTheme']='.config/sublime-text-3/Packages/User/anmol.tmTheme'
     ['sublime-text-3/Packages/User/Default.sublime-keymap']='.config/sublime-text-3/Packages/User/Default.sublime-keymap'
---
+++ scripts/terminal-launch-or-focus.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+WINDOWID=$(wmctrl -l | grep -e "Terminal$" | awk -F ' ' '{print $1}')
+if [[ -n $WINDOWID ]]; then
+    wmctrl -i -a $WINDOWID
+else
+    gnome-terminal
+fi
