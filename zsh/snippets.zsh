b4106c018bac1b1075f8222c6ec95e5bce3c66b8
Benoth <benoth83@gmail.com>
2016-06-30 13:32:00 +0200

    Add 256 colors test snippet

--- zsh/snippets.zsh
+++ zsh/snippets.zsh
@@ -14,3 +14,6 @@ function mysql:drop-tables() { mysqldump --add-drop-table --no-data $1 | grep -e
 function ssh:list()          { cat ~/.ssh/config | grep "Host " | grep -v "Host \*" | sed 's/Host //g' | sort }
 function ssh:combine()       { cp ~/.ssh/config ~/.ssh/config.bak && cat ~/.ssh/config.d/* > ~/.ssh/config } # allow for multiple ssh config files
 function ssh:keygen()        { ssh-keygen -t rsa -b 4096 -C "$1"}
+
+# Terminal snippets
+function term:test256()      { for code in $(seq -w 0 255); do for attr in 0 1; do printf "%s-%03s %bTest%b\n" "${attr}" "${code}" "\e[${attr};38;05;${code}m" "\e[m"; done; done | column -c $((COLUMNS*2)) }
