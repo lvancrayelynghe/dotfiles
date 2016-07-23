d69584539bb73f678ca094d85f7bda62ee04441e
Benoth <benoth83@gmail.com>
2016-07-23 20:16:37 +0200

    Add XFCE configs files

---
+++ xfce/NOTES.md
@@ -0,0 +1,15 @@
+# Keynames
+ampersand
+eacute
+quotedbl
+apostrophe
+parenleft
+minus
+egrave
+egrave
+underscore
+ccedilla
+agrave
+
+# Xdotool
+xdotool key --clearmodifiers super+ampersand
---
+++ xfce/restore.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p $HOME/.config/xfce4
+rsync -av --progress -h --exclude-from=$HOME/.cvsignore \
+  config/xfce4/* \
+  $HOME/.config/xfce4
+
+mkdir -p $HOME/.config/Thunar
+rsync -av --progress -h --exclude-from=$HOME/.cvsignore \
+  config/Thunar/*  \
+  $HOME/.config/Thunar
+
+mkdir -p $HOME/.gconf/apps/dockbarx
+rsync -av --progress -h --exclude-from=$HOME/.cvsignore \
+  gconf/apps/dockbarx/%gconf.xml \
+  $HOME/.gconf/apps/dockbarx/%gconf.xml
