48f416a1910091a2f43a1eef9ec13e5116a47499
Benoth <benoth83@gmail.com>
2016-07-25 23:31:06 +0200

    Fix path when restoring XFCE keyboards-shortcuts.xml

--- xfce/restore.sh
+++ xfce/restore.sh
@@ -14,3 +14,5 @@ mkdir -p $HOME/.gconf/apps/dockbarx
 rsync -av --progress -h --exclude-from=$HOME/.cvsignore \
   gconf/apps/dockbarx/%gconf.xml \
   $HOME/.gconf/apps/dockbarx/%gconf.xml
+
+sed -i -E "s|/home/[[:alnum:]]+/|$HOME/|g" $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
