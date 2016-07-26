1f87327fda191f6ed6806868c965b6df0d560ee3
Benoth <benoth83@gmail.com>
2016-07-26 09:43:17 +0200

    Fix gifify function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -343,7 +343,7 @@ function gifify() {
             time convert -verbose +dither -layers Optimize -resize 900x900\> out-static*.png  GIF:- | gifsicle --colors 128 --delay=5 --loop --optimize=3 --multifile - > $1.gif
             rm out-static*.png
         else
-            ffmpeg -i $1 -s 600x400 -pix_fmt rgb24 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > $1.gif
+            ffmpeg -i $1 -s 600x400 -pix_fmt rgb24 -r 25 -f gif - | gifsicle --optimize=3 --delay=3 > $1.gif
         fi
     else
         echo "proper usage: gifify <input_movie.mov>. You DO need to include extension."
