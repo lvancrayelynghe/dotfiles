b59b7cbf5ca4a93b97f8e8e8927f7ac9b27791b1
Benoth <benoth83@gmail.com>
2016-07-27 13:16:35 +0200

    Update XFCE keyboard shortcuts and fix switch-or-run shell script

--- scripts/switch-or-run.sh
+++ scripts/switch-or-run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# Usage: switch-app AppName AppCommand
-# Exemple: switch-app Chromium chromium-browser
+#
+# Switch/focus between existing windows or run it if it does not exists
+#
+# Usage: switch-or-run AppName AppCommand
+# Exemple: switch-or-run Chromium chromium-browser
+# Exemple: switch-or-run "([A-Za-z0-9]*)\.Thunderbird" thunderbird
+
+if [ "$#" -le 0 ]; then
+    wmctrl -lx
+    exit 0
+fi
 
 app_name=$1
 workspace_number=`wmctrl -d | grep '\*' | cut -d' ' -f 1`
@@ -9,7 +18,7 @@ workspace_number=`wmctrl -d | grep '\*' | cut -d' ' -f 1`
 # win_list=`wmctrl -lx | grep $app_name | grep " $workspace_number " | awk '{print $1}'`
 
 # Switch from all workspace
-win_list=`wmctrl -lx | grep $app_name | awk '{print $1}'`
+win_list=`wmctrl -lx | grep -E " $app_name " | awk '{print $1}'`
 
 active_win_id=`xprop -root | grep '^_NET_ACTIVE_W' | awk -F'# 0x' '{print $2}' | awk -F', ' '{print $1}'`
 if [ "$active_win_id" == "0" ]; then
@@ -18,6 +27,7 @@ fi
 
 # get next window to focus on, removing id active
 switch_to=`echo $win_list | sed s/.*$active_win_id// | awk '{print $1}'`
+
 # if the current window is the last in the list ... take the first one
 if [ "$switch_to" == "" ];then
     switch_to=`echo $win_list | awk '{print $1}'`
@@ -28,7 +38,7 @@ if [[ -n "${switch_to}" ]]
         (wmctrl -ia "$switch_to") &
     else
         if [[ -n "$2" ]]; then
-                exec $2 &
+            exec $2 &
         fi
 fi
 
