6dd4f4cb79050f6e0b4a488ad5a094ba18ebca32
Benoth <benoth83@gmail.com>
2016-08-23 13:53:54 +0200

    Add SSHFS snippets

--- zsh/snippets.zsh
+++ zsh/snippets.zsh
@@ -15,6 +15,9 @@ function mysql:drop-tables() { mysqldump --add-drop-table --no-data $1 | grep -e
 function ssh:list()          { cat ~/.ssh/config | grep "Host " | grep -v "Host \*" | sed 's/Host //g' | sort }
 function ssh:combine()       { cp ~/.ssh/config ~/.ssh/config.bak && cat ~/.ssh/config.d/* > ~/.ssh/config } # allow for multiple ssh config files
 function ssh:keygen()        { ssh-keygen -t rsa -b 4096 -C "$1"}
+function ssh:mount()         { mkdir -p "$2" 2> /dev/null ; sshfs "$1" "$2" } # $1: [user@]host:[dir] / $2: mountpoint
+function ssh:mounts()        { \ps x | grep sshfs | grep -v " grep " | awk '{$1=$2=$3=$4="";print $0}' | xargs }
+function ssh:unmount()       { fusermount -u "$1" ; rmdir "$1" 2> /dev/null } # $1: mountpoint
 
 # Terminal snippets
 function term:test256()      { for code in $(seq -w 0 255); do for attr in 0 1; do printf "%s-%03s %bTest%b\n" "${attr}" "${code}" "\e[${attr};38;05;${code}m" "\e[m"; done; done | column -c $((COLUMNS*2)) }
