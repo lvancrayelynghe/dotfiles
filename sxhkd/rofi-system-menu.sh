eea69dee344c4404488415cccc9271808c022e7f
Benoth <benoth83@gmail.com>
2016-09-26 13:18:41 +0200

    Fix systemctl scripts

--- sxhkd/rofi-system-menu.sh
+++ sxhkd/rofi-system-menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 confirm() {
-	echo -e "Yes\nNo" | rofi -dmenu -i -format d -selected-row 1 -p "${1:-Confirm: }"
+    echo -e "Yes\nNo" | rofi -dmenu -i -format d -selected-row 1 -p "${1:-Confirm: }"
 }
 
 reload="Reload sxhkd configuration"
@@ -16,16 +16,16 @@ content="$reload\n$restartpan\n$quit\n$suspend\n$reboot\n$shutdown"
 
 selection=$(echo -e $content | rofi -dmenu -i -markup-rows -p "Action: ")
 case $selection in
-	$reload)
-		pkill -USR1 -x sxhkd ;;
-	$restartpan)
-		pkill panel ; panel & ;;
-	$quit)
-		[[ $(confirm) = 1 ]] && (pkill panel ; (for win in $(bspc query -N); do bspc node $win -c ; done;) ; bspc quit) ;;
-	$suspend)
-		[[ $(confirm) = 1 ]] && (systemctl suspend) ;;
-	$reboot)
-		[[ $(confirm) = 1 ]] && (systemctl reboot) ;;
-	$shutdown)
-		[[ $(confirm) = 1 ]] && (systemctl poweroff) ;;
+    $reload)
+        pkill -USR1 -x sxhkd ;;
+    $restartpan)
+        pkill panel ; panel & ;;
+    $quit)
+        [[ $(confirm) = 1 ]] && (pkill panel ; (for win in $(bspc query -N); do bspc node $win -c ; done;) ; bspc quit) ;;
+    $suspend)
+        [[ $(confirm) = 1 ]] && (systemctl suspend -i) ;;
+    $reboot)
+        [[ $(confirm) = 1 ]] && (systemctl reboot -i) ;;
+    $shutdown)
+        [[ $(confirm) = 1 ]] && (systemctl poweroff -i) ;;
 esac
