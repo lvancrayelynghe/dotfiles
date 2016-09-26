3eb4547460aaa895a549f5417fed19aa14e2b1b9
Benoth <benoth83@gmail.com>
2016-09-27 00:01:22 +0200

    Use main or scrot to lock screen

--- scripts/screenlock.sh
+++ scripts/screenlock.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# requires imagemagick
+# requires imagemagick and maim/scrot
 
 icon="$HOME/.config/xlock/icon.png"
 tmpbg='/tmp/screenlock.png'
 
-scrot "$tmpbg"
+[[ $(hash maim > /dev/null 2>&1) ]] && maim "$tmpbg" || scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 [[ -e $icon ]] && convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -u -i "$tmpbg"
