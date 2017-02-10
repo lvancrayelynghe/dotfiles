d9d682d35401ddf02486c69255382b73dd76b8c8
Benoth <benoth83@gmail.com>
2017-02-10 15:43:00 +0100

    Fix ZSH default alias completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -9,7 +9,6 @@ compinit -i -d "${ZSH_COMPDUMP}"
 unsetopt flowcontrol     # output flow control via start/stop characters (usually assigned to ^S/^Q) is disabled in the shell’s editor
 setopt menu_complete     # autoselect the first completion entry
 setopt auto_menu         # show completion menu on succesive tab press
-setopt complete_aliases  # complete aliases
 setopt complete_in_word  # allow completion in word
 setopt always_to_end     # if a completion is performed with the cursor within a word, and a full completion is inserted, the cursor is moved to the end of the word
 
