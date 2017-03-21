8f2fb13621e6381fdba9b2c7aa64eb27a6cd81fb
Benoth <benoth83@gmail.com>
2017-03-21 23:24:30 +0100

    Update hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,15 +1,13 @@
 -- API Doc : http://www.hammerspoon.org/docs/
 -- KeyCodes : http://www.hammerspoon.org/docs/hs.keycodes.html#map
 
-hs.hotkey.bind({"ctrl"}, "return",   function() hs.application.launchOrFocus("iTerm") end)
 hs.hotkey.bind({"ctrl"}, "t",        function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind({"ctrl", "alt"}, "t", function() hs.application.launchOrFocus("iTerm") end)
 hs.hotkey.bind({"ctrl"}, "g",        function() hs.application.launchOrFocus("Google Chrome") end)
 hs.hotkey.bind({"ctrl"}, "s",        function() hs.application.launchOrFocus("Sublime Text") end)
 hs.hotkey.bind({"ctrl"}, "e",        function() hs.application.launchOrFocus("Finder") end)
 hs.hotkey.bind({"ctrl"}, "v",        function() hs.application.launchOrFocus("Filezilla") end)
-hs.hotkey.bind({"ctrl"}, "c",        function() hs.application.launchOrFocus("Calendar") end)
-hs.hotkey.bind({"ctrl"}, "p",        function() hs.application.launchOrFocus("Slack") end)
+hs.hotkey.bind({"ctrl"}, "q",        function() hs.application.launchOrFocus("Calendar") end)
+hs.hotkey.bind({"ctrl"}, "a",        function() hs.application.launchOrFocus("Slack") end)
 hs.hotkey.bind({"ctrl"}, "m",        function() hs.application.launchOrFocus("Mail") end)
 hs.hotkey.bind({"ctrl"}, "l",        function() hs.execute("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend") end)
 
