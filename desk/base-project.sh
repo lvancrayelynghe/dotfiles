a0059da8b5c4c7ee8de0b4d281686b1297dbf0df
Benoth <benoth83@gmail.com>
2017-04-27 00:34:26 +0200

    Fix desks templates

--- desk/base-project.sh
+++ desk/base-project.sh
@@ -1,10 +1,10 @@
 PROJECT_PATH=$1
 
 
-HISTFILE=$HOME/.cache/zsh-history-desk-${DESK_NAME}   # Use a per project history file
+HISTFILE=$HOME/.cache/zsh-history-desk-${DESK_NAME}  # Use a per project history file
 
 alias home="cd ${PROJECT_PATH}"                      # Go in project home
-alias dev="cd ${PROJECT_PATH}"                       # Go in project home
-alias dev-files="xdg-open ${PROJECT_PATH}"           # Browse development files
+alias dev-home="cd ${PROJECT_PATH}"                  # Go in project home
+alias dev-files="o ${PROJECT_PATH}"                  # Browse development files
 
 cd "$PROJECT_PATH"
--- desk/web-project.sh
+++ desk/web-project.sh
@@ -4,14 +4,10 @@ PROJECT_SQL=$3
 PROJECT_HOST=$4
 
 
-alias dev-http="xdg-open http://${DESK_NAME}.dev/ > /dev/null"      # Open browser on dev server
-alias dev-log="tail -f /var/log/nginx/${DESK_NAME}.dev-error.log"   # Tail nginx error log
-alias dev-mysql="mysql ${PROJECT_DB}"                               # mysql alias with dbname inserted
-alias dev-mysqldump="mysqldump --opt ${PROJECT_DB}"                 # mysqldump alias with dbname inserted
-alias dev-dumpdb="mysqldump --opt ${PROJECT_DB} > ${PROJECT_SQL}"   # Dump the database to sql file
-alias dev-restoredb="mysql -e \"DROP DATABASE \\\`${PROJECT_DB}\\\`\" &>/dev/null ; \mysql -e \"CREATE DATABASE \\\`${PROJECT_DB}\\\`\" ; mysql ${PROJECT_DB} < ${PROJECT_SQL}"   # Restore the database using the sql file
+alias dev-http="o http://${DESK_NAME}.dev/"                                                              # Open browser on dev server
 
-alias prd-http="xdg-open http://${PROJECT_HOST}/ > /dev/null"                             # Open browser on prod server
-alias prd-ftp="filezilla -c 0/${PROJECT_HOST} &"                                          # Open Filezilla on prod server
-alias prd-ssh="ssh ${PROJECT_HOST}"                                                       # Open SSH on prod server
-alias prd-getdb="ssh ${PROJECT_HOST} \"mysqldump $PRJ_NAME\" > $PRJ_PATH/sql/prod_db.sql" # Download production database
+alias prd-http="o http://${PROJECT_HOST}/"                                                               # Open browser on prod server
+alias prd-ftp="filezilla -c 0/${PROJECT_HOST} &"                                                         # Open Filezilla on prod server
+alias prd-ssh="ssh ${PROJECT_HOST}"                                                                      # Open SSH on prod server
+alias prd-getdb="ssh ${PROJECT_HOST} \"mysqldump $PRJ_NAME\" > $PRJ_PATH/.docker/db/dumps/$PRJ_NAME.sql" # Download production database
+alias prd-update="ssh ${PROJECT_HOST} \"git pull\""                                                      # Update production server
