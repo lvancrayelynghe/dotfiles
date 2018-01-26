5c30785e9d83acd93c1cebea82b79cca82de6726
Benoth <benoth83@gmail.com>
2018-01-26 17:00:24 +0100

    Add macos chrome alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -283,6 +283,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
     alias chromekill="\ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
 
+    alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
     alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
     # Ignore macos files
