fdc644b6590f6f28cd4d7648d29a6f7e9aa1df36
Benoth <benoth83@gmail.com>
2018-02-15 00:17:07 +0100

    Update Hammerspoon keybindings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,23 +1,70 @@
 -- API Doc : http://www.hammerspoon.org/docs/
 -- KeyCodes : http://www.hammerspoon.org/docs/hs.keycodes.html#map
 
-hs.hotkey.bind({"ctrl"}, "t",        function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind({"ctrl"}, "g",        function() hs.application.launchOrFocus("Google Chrome") end)
-hs.hotkey.bind({"ctrl"}, "s",        function() hs.application.launchOrFocus("Sublime Text") end)
-hs.hotkey.bind({"ctrl"}, "e",        function() hs.application.launchOrFocus("Finder") end)
-hs.hotkey.bind({"ctrl"}, "v",        function() hs.application.launchOrFocus("Filezilla") end)
-hs.hotkey.bind({"ctrl"}, "q",        function() hs.application.launchOrFocus("Calendar") end)
-hs.hotkey.bind({"ctrl"}, "a",        function() hs.application.launchOrFocus("Slack") end)
-hs.hotkey.bind({"ctrl"}, "m",        function() hs.application.launchOrFocus("Mail") end)
-hs.hotkey.bind({"ctrl"}, "b",        function() hs.application.launchOrFocus("Sequel Pro") end)
-hs.hotkey.bind({"ctrl"}, "l",        function() hs.execute("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend") end)
-hs.hotkey.bind({"ctrl"}, "p",        function() hs.execute("/usr/local/bin/subl /Users/lucvancrayelynghe/TODO.md") end)
+-- Force launch or focus an app by name (fix issues with Finder)
+-- @see https://github.com/cmsj/hammerspoon-config/blob/master/init.lua#L156
+-- @see http://liuhao.im/english/2017/06/02/macos-automation-and-shortcuts-with-hammerspoon.html
+function launchOrFocusOrHide(appname)
+    local app = hs.appfinder.appFromName(appname)
+    if not app then
+        if appname == "Calendrier" then
+            hs.application.launchOrFocus("Calendar")
+        else
+            hs.application.launchOrFocus(appname)
+        end
+        return
+    end
+
+    if appname == "Finder" then
+        app:activate(true)
+        app:unhide()
+        return
+    end
+
+    local mainwin = app:mainWindow()
+    if mainwin then
+        if mainwin == hs.window.focusedWindow() then
+            app:hide()
+        else
+            app:activate(true)
+            app:unhide()
+            mainwin:focus()
+        end
+    end
+end
+
+-- Set a screen grid size
+hs.grid.setGrid('12x12')
+hs.grid.setMargins('0x0')
+
+-- Key bindings
+hs.hotkey.bind({"ctrl"}, "t", function() launchOrFocusOrHide("iTerm") end)
+hs.hotkey.bind({"ctrl"}, "g", function() launchOrFocusOrHide("Google Chrome") end)
+hs.hotkey.bind({"ctrl"}, "s", function() launchOrFocusOrHide("Sublime Text") end)
+hs.hotkey.bind({"ctrl"}, "e", function() launchOrFocusOrHide("Finder") end)
+hs.hotkey.bind({"ctrl"}, "v", function() launchOrFocusOrHide("Filezilla") end)
+hs.hotkey.bind({"ctrl"}, "q", function() launchOrFocusOrHide("Calendrier") end)
+hs.hotkey.bind({"ctrl"}, "a", function() launchOrFocusOrHide("Slack") end)
+hs.hotkey.bind({"ctrl"}, "b", function() launchOrFocusOrHide("Sequel Pro") end)
+hs.hotkey.bind({"ctrl"}, "m", function() hs.execute("/System/Library/CoreServices/Menu\\ Extras/User.menu/Contents/Resources/CGSession -suspend") end)
+hs.hotkey.bind({"ctrl"}, "p", function() hs.execute("/usr/local/bin/subl /Users/lucvancrayelynghe/TODO.md") end)
+-- hs.hotkey.bind({"ctrl"}, "m", function() launchOrFocusOrHide("Mail") end)
+
+hs.hotkey.bind({"ctrl"}, "h", function() hs.grid.resizeWindowShorter(hs.window.focusedWindow()) end)
+hs.hotkey.bind({"ctrl"}, "j", function() hs.grid.resizeWindowThinner(hs.window.focusedWindow()) end)
+hs.hotkey.bind({"ctrl"}, "k", function() hs.grid.resizeWindowWider(hs.window.focusedWindow()) end)
+hs.hotkey.bind({"ctrl"}, "l", function() hs.grid.resizeWindowTaller(hs.window.focusedWindow()) end)
+
+hs.hotkey.bind({"ctrl", "cmd"}, "h", function() hs.grid.pushWindowUp(hs.window.focusedWindow()) end)
+hs.hotkey.bind({"ctrl", "cmd"}, "j", function() hs.grid.pushWindowLeft(hs.window.focusedWindow()) end)
+hs.hotkey.bind({"ctrl", "cmd"}, "k", function() hs.grid.pushWindowRight(hs.window.focusedWindow()) end)
+hs.hotkey.bind({"ctrl", "cmd"}, "l", function() hs.grid.pushWindowDown(hs.window.focusedWindow()) end)
 
 -- Toggle Fullscreen
-hs.hotkey.bind({"ctrl"}, "f", function()
-  local win = hs.window.focusedWindow()
-  win:toggleFullScreen()
-end)
+-- hs.hotkey.bind({"ctrl"}, "f", function()
+--   local win = hs.window.focusedWindow()
+--   win:toggleFullScreen()
+-- end)
 
 -- Close active window
 -- hs.hotkey.bind({"ctrl"}, "x", function()
